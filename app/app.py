87bbf73ec0b32065d9fdcb7008f1ac6dcc3ea9ca
Mardugo <84402075+Mardugo@users.noreply.github.com>
2025-03-26 14:51:09 -0500

    fixing bugs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
