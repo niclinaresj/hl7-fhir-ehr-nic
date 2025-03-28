f843e2d4055215b80234442a245f7755c2835721
Mardugo <84402075+Mardugo@users.noreply.github.com>
2025-03-28 15:19:52 -0500

    bugfix

--- app/app.py
+++ app/app.py
@@ -25,7 +25,7 @@ async def get_patient_by_id(patient_id: str):
 
 @app.get("/patient", response_model=dict)
 async def get_patient_by_identifier(system: str, value: str):
-    print("solicitud datos:",systm,value)
+    print("solicitud datos:",system,value)
     status,patient = GetPatientByIdentifier(system,value)
     if status=='success':
         return patient  # Return patient
